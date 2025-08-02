c96a928a406b8bb29976b5a1217a37c68eb5ea5c
TefyerDevs <tefyergames2@gmail.com>
2025-08-02 15:54:38 -0400

    forgot to add.

--- src/main/java/org/labrat/torrentofwar/registry/CreativeModeTabRegistry.java
+++ src/main/java/org/labrat/torrentofwar/registry/CreativeModeTabRegistry.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.labrat.torrentofwar.TorrentOfWarMod;
 import org.labrat.torrentofwar.api.ores.StoneType;
